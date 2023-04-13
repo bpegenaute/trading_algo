11367fa7ba58badcffcdbb77eef814faac584834
Blas Pegenaute <>
2023-04-13 20:38:13 +0200

    improv

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,52 +1,62 @@
 import random
 import numpy as np
 import torch
-import torch.nn.functional as F
-import torch.nn as nn
-import torch.optim as optim
 from collections import deque
-from model.lstm_model import LSTMModel
 
 class DQNAgent:
-    def __init__(self, state_size, action_size, hidden_size=64, num_layers=2):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=1000)
+    def __init__(self, env, model, target_model, device):
+        self.env = env
+        self.model = model
+        self.target_model = target_model
+        self.device = device
+        self.memory = deque(maxlen=2000)
+
         self.gamma = 0.95
         self.epsilon = 1.0
-        self.epsilon_min = 0.01
+        self.epsilon_min = 0
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
-        self.model = LSTMModel(state_size, hidden_size, num_layers, action_size)
-        self.target_model = LSTMModel(state_size, hidden_size, num_layers, action_size)
+        self.model.to(device)
+        self.target_model.to(device)
+        self.target_model.load_state_dict(self.model.state_dict())
+        self.target_model.eval()
+        self.criterion = torch.nn.MSELoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        self.update_target_model()
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
-    def act(self, state):
+    def act(self, state, sentiment_scores):
         if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model(torch.tensor(state, dtype=torch.float).unsqueeze(0))
+            return random.randrange(self.env.action_space.n)
+
+        state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0).to(self.device)
+        sentiment_tensor = torch.tensor(sentiment_scores, dtype=torch.float32).unsqueeze(0).to(self.device)
+        act_values = self.model(torch.cat((state_tensor, sentiment_tensor), dim=2))
         return torch.argmax(act_values).item()
 
+    def memorize(self, state, action, reward, next_state, done, sentiment_scores):
+        self.memory.append((state, action, reward, next_state, done, sentiment_scores))
+
     def replay(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+
         minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = reward
-            if not done:
-                next_state_tensor = torch.tensor(next_state, dtype=torch.float).unsqueeze(1).unsqueeze(0)
-                target = (reward + self.gamma * torch.max(self.target_model(next_state_tensor)).item())
-            state_tensor = torch.tensor(state, dtype=torch.float).unsqueeze(1).unsqueeze(0)
-            target_f = self.model(state_tensor)
+
+        for state, action, reward, next_state, done, sentiment_scores in minibatch:
+            state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0).to(self.device)
+            next_state_tensor = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0).to(self.device)
+            sentiment_tensor = torch.tensor(sentiment_scores, dtype=torch.float32).unsqueeze(0).to(self.device)
+
+            target = (reward + self.gamma * torch.max(self.target_model(torch.cat((next_state_tensor, sentiment_tensor), dim=2))).item()) if not done else reward
+            target_f = self.model(torch.cat((state_tensor, sentiment_tensor), dim=2))
             target_f[0][action] = target
+
             self.optimizer.zero_grad()
-            loss = F.mse_loss(self.model(torch.tensor(state, dtype=torch.float).unsqueeze(1).unsqueeze(0)), target_f)
+            loss = self.criterion(target_f, self.model(torch.cat((state_tensor, sentiment_tensor), dim=2)))
             loss.backward()
             self.optimizer.step()
+
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
-    def update_target_model(self):
+    def retrain(self):
         self.target_model.load_state_dict(self.model.state_dict())
--- main.py
+++ main.py
@@ -1,55 +1,58 @@
 import os
-import sys
+import pandas as pd
 import numpy as np
-import yfinance as yf
-from datetime import datetime
-from environment.trading_env import TradingEnvironment
-from dqn_agent import DQNAgent
-from config import OPENAI_API_KEY, BING_API_KEY
-from news_fetcher import fetch_news
-from sentiment import analyze_sentiment
-from openai_api import OpenAIAPI
+import torch
+from config import *
+from agent.environment import TradingEnvironment
+from agent.model.lstm_model import LSTMModel
+from agent.dqn_agent import DQNAgent
+from agent.news_fetcher import fetch_news
+from agent.sentiment import get_sentiment
 
-symbol = "AAPL"
-start_date = "2010-01-01"
-end_date = datetime.today().strftime("%Y-%m-%d")
-
-# Fetch historical stock data
-data = yf.download(symbol, start=start_date, end=end_date)
-data.reset_index(inplace=True)
-
-# Fetch news articles
-news = fetch_news(BING_API_KEY, symbol)
-
-# Process news articles using OpenAI API
-openai_api = OpenAIAPI(OPENAI_API_KEY)
-news_summary = openai_api.generate_summary(news)
-
-# Analyze sentiment
-sentiment = analyze_sentiment(news_summary)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Create the trading environment
-env = TradingEnvironment(data, sentiment)
-state_size = env.observation_space.shape[0]
-action_size = env.action_space.n
+data = pd.read_csv('AAPL.csv')
+env = TradingEnvironment(data)
 
-# Initialize the DQN agent
-agent = DQNAgent(state_size, action_size)
+# Parameters for the LSTM model
+input_size = 61
+hidden_size = 64
+num_layers = 2
+output_size = 3
 
-# Train the agent
-EPISODES = 1000
-for e in range(EPISODES):
+# Instantiate the LSTM model and the target model
+model = LSTMModel(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, output_size=output_size, device=device)
+target_model = LSTMModel(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, output_size=output_size, device=device)
+
+# Create the DQN agent
+agent = DQNAgent(env, model, target_model, device)
+
+# Training parameters
+n_episodes = 1000
+train_frequency = 10
+
+for e in range(n_episodes):
     state = env.reset()
-    state = np.reshape(state, [1, state_size, 6])
     done = False
+    score = 0
+    
     while not done:
-        action = agent.act(state)
-        next_state, reward, done, _ = env.step(action)
-        next_state = np.reshape(next_state, [1, state_size, 6])
-        agent.remember(state, action, reward, next_state, done)
+        # Fetch the news
+        news_data = fetch_news()
+        # Perform sentiment analysis
+        sentiment_scores = get_sentiment(news_data)
+
+        action = agent.act(state, sentiment_scores)
+        next_state, reward, done = env.step(action)
+        agent.memorize(state, action, reward, next_state, done, sentiment_scores)
+        
         state = next_state
-        if done:
-            print(f"episode: {e}/{EPISODES}, score: {env.score}, e: {agent.epsilon:.2}")
-            agent.update_target_model()
-    if len(agent.memory) > 32:
-        agent.replay(32)
+        score += reward
+
+    print(f"episode: {e}/{n_episodes}, score: {score}, e: {agent.epsilon}")
+
+    agent.replay(32)
+    # Retrain the model
+    if e % train_frequency == 0:
+        agent.retrain()
