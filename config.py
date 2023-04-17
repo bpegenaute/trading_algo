eb4c36c55199b1d408f518b987e1b30fc78aa794
Blas Pegenaute <>
2023-04-17 14:21:10 +0200

    print + config

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.episodes = 1
-        self.window_size = 10
-        self.batch_size = 32
-        self.memory_capacity = 1000000
+        self.episodes = 500
+        self.window_size = 15
+        self.batch_size = 64
+        self.memory_capacity = 500000
         self.epsilon_min = 0.01
         self.epsilon_start = 1.0
         self.epsilon_end = 0.01
         self.epsilon_decay = 0.995
-        self.gamma = 0.95
+        self.gamma = 0.99
         self.learning_rate = 0.001
         self.target_network_update_interval = 1000
         self.IB_PORT = int(os.getenv("IB_PORT"))
